5ac5e254065fb337149b03512cdd7318d82791c4
Raviprasath R <raviprasath.in@gmail.com>
2023-09-01 10:03:10 +0530

    Login and Register frontend and backend linking

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/pages/Login.scss";
 import { Link } from "react-router-dom";
 import HomeNavbar from "../components/HomeNavbar";
 import Footer from "../components/Footer";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8000/api/users/login`, {
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        const res = JSON.stringify(result.data);
+        localStorage.setItem("Data", res);
+
+        if (result.data.login !== "false") {
+          navigate("/home");
+          document.location.reload();
+        } else {
+          setErrorMsg(result.data.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <HomeNavbar />
       <section id="login">
         <h3>Login to your account</h3>
-        <form>
+        <form onSubmit={handleLoginSubmit}>
           <label htmlFor="email">Email: </label>
           <input
             id="email"
             name="email"
             type="email"
+            value={email}
             placeholder="you@mail.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor="password">Password: </label>
           <input
             id="password"
             name="password"
+            value={password}
             type="password"
             placeholder="*********"
+            onChange={(e) => setPassword(e.target.value)}
           />
+          <p className="error-msg">{errorMsg}</p>
           <Link to="/forgot-password">Forgot Password?</Link>
           <button className="btn" type="submit">
             Login
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/pages/Register.scss";
 import { Link } from "react-router-dom";
 import HomeNavbar from "../components/HomeNavbar";
 import Footer from "../components/Footer";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegisterSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8000/api/users/register`, {
+        name: name,
+        phone: phone,
+      })
+      .then((result) => {
+        const res = JSON.stringify(result.data);
+        localStorage.setItem("Data", res);
+        navigate("/dashboard");
+        document.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <HomeNavbar />
       <section id="register">
         <h3>Create an account</h3>
-        <form>
+        <form onSubmit={handleRegisterSubmit}>
           <label htmlFor="email">Name: </label>
-          <input id="name" name="name" type="text" placeholder="John Doe" />
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="John Doe"
+          />
           <label htmlFor="email">Phone: </label>
-          <input id="phone" name="phone" type="tel" placeholder="9876543210" />
+          <input
+            id="phone"
+            name="phone"
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="9876543210"
+          />
           <label htmlFor="email">Email: </label>
           <input
             id="email"
             name="email"
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="you@mail.com"
           />
           <label htmlFor="password">Create Password: </label>
@@ -27,6 +69,8 @@ const Register = () => {
             id="password"
             name="password"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="*********"
           />
           <button className="btn" type="submit">
